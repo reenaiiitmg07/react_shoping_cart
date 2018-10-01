d583794beb15a2e10e60f52b195bbe04fb3aa558
Reena Verma <reena@kolkaz.strandls.com>
2018-10-01 13:54:18 +0530

    update

--- src/action/index.js
+++ src/action/index.js
@@ -1,7 +1,14 @@
 export const SET_DATA='SET_DATA';
+export const SET_SIZE='SET_SIZE';
 export function setProductData(data){
   return{
     type:SET_DATA,
+    payload:data
+  }
+}
+export function setSizeData(data){
+  return{
+    type:SET_SIZE,
     payload:data
   }
 }
--- src/components/selectedsize.js
+++ src/components/selectedsize.js
@@ -8,9 +8,11 @@ class Selsize extends Component {
       sizes:[]
     }
     this.showSize=this.showSize.bind(this);
+    this.selectSize=this.selectSize.bind(this);
     }
     showSize(e){
       let size = new Set();
+      //console.log(this.props.data);
       this.props.data.map((item)=>{
        let asize=[];
        asize=item['availableSizes'];
@@ -18,8 +20,13 @@ class Selsize extends Component {
          size.add(x);
        })
       })
-      console.log(size);
      this.setState({sizes:size});
+  }
+  selectSize(e){
+    console.log(e.target.value);
+    let avlProd=this.props.data;
+    console.log(avlProd);
+
   }
   render(){
     let array=[...this.state.sizes];
@@ -29,7 +36,7 @@ class Selsize extends Component {
       <div className="row">
       {array.map((value)=>{
         return(
-        <div className="col-sm-3"style={{padding:"5px 0px 0px 0px"}}><button className="btn btn-primary">{value}</button></div>
+        <div className="col-sm-3"style={{padding:"5px 0px 0px 0px"}}><button onClick={this.selectSize} value={value} className="btn btn-primary">{value}</button></div>
       )
       })}
       </div>
